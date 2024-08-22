24bff70b6f7a58278b8fd0d9a5ed008e3075e845
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-08-22 11:58:19 -0300

    Creacion de clase
    Creé la nueva clase y la setee para poder usar arraylists y demas

---
+++ src/EJERCICIO1/mainEjercicio1_a.java
@@ -0,0 +1,15 @@
+package EJERCICIO1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class mainEjercicio1_a {
+
+	public static void main(String[] args) {
+		ArrayList<Profesor> profesores = new ArrayList<>();
+		
+		
+
+	}
+
+}
