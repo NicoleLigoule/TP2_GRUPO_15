5b87e0f609abfcd76c26368ce59f6a2e44121408
Nicole <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-08-24 17:23:55 -0300

    ArrayList cargado e iterador para mostrar profesores

--- src/EJERCICIO1/mainEjercicio1_a.java
+++ src/EJERCICIO1/mainEjercicio1_a.java
@@ -1,7 +1,7 @@
 package EJERCICIO1;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.ListIterator;
 
 
 public class mainEjercicio1_a {
@@ -15,11 +15,18 @@ public class mainEjercicio1_a {
 		prof[2]=new Profesor("Ramiro",32,"Direcor",8);
 		prof[3]=new Profesor("Manuel",42,"Docente",13);
 		prof[4]=new Profesor("Felipe",52,"Preceptor",25);
-
 		
+		profesores.add(prof[0]);
+		profesores.add(prof[1]);
+		profesores.add(prof[2]);
+		profesores.add(prof[3]);
+		profesores.add(prof[4]);
 		
-		
-		
+		ListIterator<Profesor> iterador = profesores.listIterator();
+		 while(iterador.hasNext()){
+			Profesor profesor = iterador.next();
+			iterador.remove();
+			System.out.println(profesor.toString());	
+		}
 	}
-
 }
