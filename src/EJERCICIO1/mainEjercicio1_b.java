b65962665b32962d52134880991a84e40bd0786e
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-08-25 20:36:49 -0300

    Agregue la clase llamada mainEjercicio1_b  y cree los 5 profesores

---
+++ src/EJERCICIO1/mainEjercicio1_b.java
@@ -0,0 +1,23 @@
+package EJERCICIO1;
+
+import java.util.TreeSet;
+
+public class mainEjercicio1_b {
+	public static void main(String[] args) {
+		///(String nombre,int edad,String cargo,int antiguedadDocente) 
+		Profesor[] prof = new Profesor[5];
+		prof[0]=new Profesor("juan",26,"Docente",2);
+		prof[1]=new Profesor("Tomas",46,"Preceptor",10);
+		prof[2]=new Profesor("Ramiro",32,"Direcor",8);
+		prof[3]=new Profesor("Manuel",42,"Docente",13);
+		prof[4]=new Profesor("Felipe",52,"Preceptor",25);
+		TreeSet<Profesor> profesores = new TreeSet<>();
+		profesores.add(prof[0]);
+		profesores.add(prof[1]);
+		profesores.add(prof[2]);
+		profesores.add(prof[3]);
+		profesores.add(prof[4]);
+
+	}
+	
+}
