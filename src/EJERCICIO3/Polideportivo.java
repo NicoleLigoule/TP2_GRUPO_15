a6476f3fd60e9cd2154a129d96b4fc779b2f3545
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-26 10:02:47 -0300

    Clase Polideportivo

---
+++ src/EJERCICIO3/Polideportivo.java
@@ -0,0 +1,13 @@
+package EJERCICIO3;
+
+public class Polideportivo extends Edificio implements instalacionDeportiva{
+
+	private String nombre;
+	
+	@Override
+	public int getTipoDeInstalacion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
