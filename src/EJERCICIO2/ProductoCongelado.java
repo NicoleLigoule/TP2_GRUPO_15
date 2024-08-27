269973a055b8f4a0bfcde9b57b5bec3979ee466b
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-08-27 20:28:07 -0300

    Final clase producto congelado

--- src/EJERCICIO2/ProductoCongelado.java
+++ src/EJERCICIO2/ProductoCongelado.java
@@ -9,6 +9,11 @@ public class ProductoCongelado extends Producto {
 	        super();
 	        this.temperatura_c =0;
 	    }
+	  	
+	  	public ProductoCongelado(String fecha_de_caducidad,int numero_de_lote,int Temp) {
+	  		super(fecha_de_caducidad,numero_de_lote);
+	  		this.temperatura_c=Temp;
+	  	}
 	  
 	    public int getTemperatura_c() {
 	        return temperatura_c;
@@ -16,6 +21,10 @@ public class ProductoCongelado extends Producto {
 
 	    public void setTemperatura_c(int temperatura_c) {
 	        this.temperatura_c = temperatura_c;
+	    }
+	    @Override
+	    public String toString() {
+	    	return "temperatura de congelación recomendada:" + temperatura_c +" "+ super.toString();
 	    }
 
 }
