2d53dc5ca3c30ac7898d3c82abed47ed85fad16f
Nicole <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-08-27 14:41:15 -0300

    Clase ProductoCongelado, constructor x def, set y get

--- src/EJERCICIO2/ProductoCongelado.java
+++ src/EJERCICIO2/ProductoCongelado.java
@@ -2,4 +2,20 @@ package EJERCICIO2;
 
 public class ProductoCongelado extends Producto {
 
+	  private int temperatura_c;
+
+	  //Constructor
+	  	public ProductoCongelado() {
+	        super();
+	        this.temperatura_c =0;
+	    }
+	  
+	    public int getTemperatura_c() {
+	        return temperatura_c;
+	    }
+
+	    public void setTemperatura_c(int temperatura_c) {
+	        this.temperatura_c = temperatura_c;
+	    }
+
 }
