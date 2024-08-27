61e4751e277db91a75c09fa0a1cd2070834b2745
danimrios <danimrios09@gmail.com>
2024-08-27 12:53:16 -0300

    clase edificio de oficina
    cree la clase falta metodo tostring

---
+++ src/EJERCICIO3/EdificioDeOficina.java
@@ -0,0 +1,23 @@
+package EJERCICIO3;
+
+public class EdificioDeOficina extends Edificio {
+private int NumeroDeOficina;
+
+public int getNumeroDeOficina() {
+	return NumeroDeOficina;
+}
+
+public void setNumeroDeOficina(int numeroDeOficina) {
+	NumeroDeOficina = numeroDeOficina;
+}
+
+public EdificioDeOficina() {
+	super();
+	this.NumeroDeOficina=-1;
+}
+
+public EdificioDeOficina(double superficie,int oficinas) {
+	super(superficie);
+	this.NumeroDeOficina=oficinas;
+}
+}
