ca1ea1d460ac6be500f3080952efd507dacce0c8
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-27 21:34:41 -0300

    Clase Polideportivo
    constructores, get y set

--- src/EJERCICIO3/Polideportivo.java
+++ src/EJERCICIO3/Polideportivo.java
@@ -4,10 +4,34 @@ public class Polideportivo extends Edificio implements instalacionDeportiva{
 
 	private String nombre;
 	
+	//Constructores
+	public Polideportivo() {
+		super();
+		
+	}
+	
+	public Polideportivo(double superficie, String nombre) {
+		super(superficie);
+		this.nombre = nombre;
+	}
+		
+	//Getter/Setters
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
 	@Override
 	public int getTipoDeInstalacion() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+
 
 }
