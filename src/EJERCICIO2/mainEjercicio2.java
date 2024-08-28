92afb2d5f67e170a6edfe8d417c23ab323dc73e7
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-08-28 00:58:18 -0300

    agregue objeto de la clase ProductoFresco a la clase main

--- src/EJERCICIO2/mainEjercicio2.java
+++ src/EJERCICIO2/mainEjercicio2.java
@@ -5,7 +5,7 @@ public class mainEjercicio2 {
 	public static void main(String[] args) {
 		
 		ProductoCongelado pc1 = new ProductoCongelado("01/01/2025", 100001, -10);
-		
+		ProductoFresco Fresco=new ProductoFresco("12/10/2026",1,"Bolsa","argentina");
 		
 
 	}
