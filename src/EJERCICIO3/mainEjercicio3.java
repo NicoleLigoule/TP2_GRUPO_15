172a2a7355b4ee7797c07822107834f799135baf
Nicole <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-08-29 22:41:48 -0300

    Agrego 2 registros

--- src/EJERCICIO3/mainEjercicio3.java
+++ src/EJERCICIO3/mainEjercicio3.java
@@ -15,8 +15,8 @@ public class mainEjercicio3 {
 		edificios[0]= new Polideportivo(34,"ferro",3);
 		edificios[1]= new Polideportivo(120,"River",1);
 		edificios[2]= new Polideportivo(300,"Estudiantes",2); 
-		edificios[3]= new EdificioDeOficina(500, 10);
-        edificios[4]= new EdificioDeOficina(800, 20);
+		edificiosList.add(new EdificioDeOficina(600, 15));
+        edificiosList.add(new EdificioDeOficina(950, 30));
 
 		edificiosList.add(edificios[0]);
 
