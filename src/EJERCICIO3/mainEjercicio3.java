74d73959198f0ff0e5c54ff8a2f7e0d6dbbfdddf
Nicole <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-08-29 22:21:49 -0300

    Agrego los 2 obj edificio, el arraylist y cargo 1 registro

--- src/EJERCICIO3/mainEjercicio3.java
+++ src/EJERCICIO3/mainEjercicio3.java
@@ -3,15 +3,23 @@ package EJERCICIO3;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
+import EJERCICIO1.Profesor;
+
 
 public class mainEjercicio3 {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Edificio> edificiosList = new ArrayList<>();
 		Edificio[] edificios = new Edificio[5];
-		edificios[0]=new Polideportivo(34,"ferro",3);
-		edificios[1]=new Polideportivo(120,"River",1);
-		edificios[2]=new Polideportivo(300,"Estudiantes",2);
+		edificios[0]= new Polideportivo(34,"ferro",3);
+		edificios[1]= new Polideportivo(120,"River",1);
+		edificios[2]= new Polideportivo(300,"Estudiantes",2); 
+		edificios[3]= new EdificioDeOficina(500, 10);
+        edificios[4]= new EdificioDeOficina(800, 20);
+
+		edificiosList.add(edificios[0]);
+
 
 	}
 
